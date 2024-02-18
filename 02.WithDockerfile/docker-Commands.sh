6308b6909dabd433c2af18128d61da2419bfa777
mujahed85 <mujahed.mca@gmail.com>
2024-02-18 13:08:15 +0530

    Updated werkzeug package in Dockerfile for Flask Web Python Application

--- 02.WithDockerfile/docker-Commands.sh
+++ 02.WithDockerfile/docker-Commands.sh
@@ -7,13 +7,12 @@ docker run --name mysql-container -e DB_HOST=localhost -e DB_USER=root -e DB_NAM
 docker exec -it mysql-container mysql -uroot -p
 
 
-
-docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql-db
-docker run -d -p 5000:5000 --name flask-app -e DB_HOST=172.17.0.2 -e DB_USER=db_user -e DB_PASSWORD=123 -e DB_NAME=dev1 -e TABLE_NAME=users flask-app
+ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql-db)
+docker run -d -p 5000:5000 --name flask-app -e DB_HOST=$ip -e DB_USER=db_user -e DB_PASSWORD=123 -e DB_NAME=dev1 -e TABLE_NAME=users flask-app
 
 
 docker exec -it mysql-container mysql -uroot -p123
 docker exec -it flask-app bash
 
-
-curl http://localhost:5000
+docker logs flask-app
+curl http://$ip:5000
