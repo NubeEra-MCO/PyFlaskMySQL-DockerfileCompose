833ac4ee3dcac0c8766b3016d06e9d9ff0301e27
mujahed85 <mujahed.mca@gmail.com>
2024-02-18 12:55:29 +0530

    Dockerfile Commands Added in shell scripting

--- 02.WithDockerfile/docker-Commands.sh
+++ 02.WithDockerfile/docker-Commands.sh
@@ -9,7 +9,11 @@ docker exec -it mysql-container mysql -uroot -p
 
 
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql-db
-docker run -d -p 5000:5000 --name flask-app -e DB_HOST=<MySQL_Container_IP> -e DB_USER=db_user -e DB_PASSWORD=db_password -e DB_NAME=dev1 -e TABLE_NAME=users flask-app
+docker run -d -p 5000:5000 --name flask-app -e DB_HOST=172.17.0.2 -e DB_USER=db_user -e DB_PASSWORD=123 -e DB_NAME=dev1 -e TABLE_NAME=users flask-app
+
+
+docker exec -it mysql-container mysql -uroot -p123
+docker exec -it flask-app bash
 
 
 curl http://localhost:5000
